435abf70c52c0d2a6d1d8d6ed21184dc83128b5b
LeoSilvaGomes <leonardodasigomes@gmail.com>
2020-03-04 17:55:58 -0300

    #1 Create the template structure

--- src/encurtador/urls.py
+++ src/encurtador/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
     path('', views.new_link, name='new_link')
 ]
--- src/encurtador/views.py
+++ src/encurtador/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from .models import Link
+from .forms import LinkForm
 
 def new_link(request):
     '''
