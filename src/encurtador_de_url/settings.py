b00b71c5833dd8fbe06a042f5cd63bacea6ed7ee
LeoSilvaGomes <leonardodasigomes@gmail.com>
2020-03-05 08:27:39 -0300

    #1 Adjust folders structure

--- src/encurtador_de_url/settings.py
+++ src/encurtador_de_url/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'encurtador_de_url.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,5 +121,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-   os.path.join(BASE_DIR, 'static'),
+   os.path.join(BASE_DIR, 'templates/static'),
 )
