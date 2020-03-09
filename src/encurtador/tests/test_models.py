3ed1eae0349a3678f0ea8f2ea93e8c117b0c05d6
LeoSilvaGomes <leonardodasigomes@gmail.com>
2020-03-09 16:58:17 -0300

    #1 Add test to get_shortener view method

--- src/encurtador/tests/test_models.py
+++ src/encurtador/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from encurtador.models import Link
+from encurtador.forms import LinkForm
 
 
 class TestLink(TestCase):
@@ -13,5 +14,25 @@ class TestLink(TestCase):
     def test_url(self):        
         self.assertEqual(
             str(self.link),
-            ('https://gitlab.com/pencillabs')
+            ('https://gitlab.com/pencillabs : 1')
         )
+    
+    def test_to_short_url(self):
+        self.assertEqual(
+            self.link.to_short_url(),
+            '867nv'
+        )
+
+    def test_to_id(self):
+        self.assertEqual(
+            Link.to_id('867nv'),
+            (1)
+        )
+
+class TestFormLink(TestCase):
+
+    def setUp(self):
+        self.form = LinkForm(data={'url' : 'https://gitlab.com/pencillabs'})
+
+    def test_form_valid(self):
+        self.assertTrue(self.form.is_valid())
--- src/encurtador/tests/test_views.py
+++ src/encurtador/tests/test_views.py
@@ -7,9 +7,9 @@ from encurtador.views import LinkForm
 class TestNewLink(TestCase):
 
     def setUp(self):
-        self.client     = Client()
-        self.new_link   = reverse('encurtador:new_link')
-        self.link = Link.objects
+        self.client         = Client()
+        self.new_link       = reverse('encurtador:new_link')
+        self.link           = Link.objects
 
     def test_new_link_GET(self):
         response    = self.client.get(self.new_link)
@@ -18,9 +18,28 @@ class TestNewLink(TestCase):
         self.assertTemplateUsed(response, 'encurtador/home.html')
 
     def test_new_link_POST(self):
-        response = self.client.post(self.new_link, {
+        response    = self.client.post(self.new_link, {
             'url'   : 'https://gitlab.com/pencillabs/encurtador-de-url' 
         })
 
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(self.link.get(id=1).url, 'https://gitlab.com/pencillabs/encurtador-de-url')
+
+class TestGetShortener(TestCase):
+    
+    def setUp(self):
+        self.client         = Client()
+        self.new_link       = reverse('encurtador:new_link')
+        self.client.post(self.new_link, {'url' : 'https://gitlab.com/pencillabs/encurtador-de-url'})
+
+    def test_get_shortener_GET(self):
+        self.get_shortener  = (reverse('encurtador:get_shortener', args=['867nv']))
+        response    = self.client.get(self.get_shortener)
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_get_shortener_get(self):
+        self.get_shortener  = (reverse('encurtador:get_shortener', args=['867n3']))
+        response    = self.client.get(self.get_shortener)
+
+        self.assertEqual(response.status_code, 404)
